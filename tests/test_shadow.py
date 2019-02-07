ab5419b18a51cdf75537b99e58e5f928acd766c4
Bobby <karmma0@gmail.com>
2019-02-06 17:48:03 -0700

    Fixed initial end-to-end tests

--- tests/test_shadow.py
+++ tests/test_shadow.py
@@ -18,39 +18,50 @@ def test_command_line_interface():
     assert result.exit_code == 0
     assert 'Usage: main ' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
     assert 'Console script for shadow.' in help_result.output
+    assert help_result.exit_code == 0
 
 
-def _test_cli_sim(tmpdir):
+def test_cli_sim(tmpdir):
     """Test the CLI."""
     tmpfile = tmpdir.join("test.txt.tpl")
+    tmpfile.write('asht {{test}}\n')
+
     runner = CliRunner()
-    result = runner.invoke(cli.main, ['sim', 'test.txt.tpl'])
-    #assert result.exit_code == 0
-    assert 'Using current working directory.' in result.output
+    result = runner.invoke(cli.main, ['sim', str(tmpfile)])
+    assert f"Generating template: {str(tmpfile)}" in result.output
+    assert result.exit_code == 0
 
 
 def test_cli_fax(tmpdir):
     """Test the CLI."""
     tmpsource = tmpdir.join('test.txt.tpl')
-    print(f"{tmpsource}")
     tmpsource.write('asht {{test}}\n')
     tmpdest = tmpdir.join('test.txt')
-    print(f"{tmpdest}")
-
 
     runner = CliRunner()
-    result = runner.invoke(cli.main, ['fax', tmpsource])
-    assert result == ''
-    #assert result.exit_code == 0
-    assert tmpdest.read() == 'asht '
-
-
-def _test_cli_clean(tmpdir):
-    """Test the CLI."""
-    tmpfile = tmpdir.join("test.txt.tpl")
-    runner = CliRunner()
-    result = runner.invoke(cli.main, ['clean'])
+    result = runner.invoke(cli.main, ['fax', '-C', '{"test": "thsa"}', str(tmpsource)])
+    assert tmpdest.read() == 'asht thsa'
     assert result.exit_code == 0
-    assert 'Using current working directory.' in result.output
+
+
+def test_cli_clean(tmpdir):
+    """Test the CLI."""
+    tmpsource = tmpdir.join('test.txt.tpl')
+    tmpsource.write('asht {{test}}\n')
+    tmpdest = tmpdir.join('test.txt')
+
+    runner = CliRunner()
+    result = \
+        runner.invoke(cli.main,
+                ['fax', '-C', '{"test": "thsa"}', str(tmpsource)])
+    assert tmpdest.read() == 'asht thsa'
+    assert result.exit_code == 0
+
+    runner = CliRunner()
+    result = runner.invoke(cli.main, ['clean', str(tmpsource)])
+    assert f"Cleaning generated file: {str(tmpdest)}" in result.output
+    assert result.exit_code == 0
+
+    assert tmpsource.exists()
+    assert not tmpdest.exists()
